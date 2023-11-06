00e30d394f2774fd257ccd1f3a1a561071e9b077
hieu <doclang0801@gmail.com>
2023-11-06 09:25:26 +0700

    refactor: sửa app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-slate-500">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline bg-slate-500">
+        Add new test
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
